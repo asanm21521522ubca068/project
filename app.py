5dceddfc1e8fb26340bc594f4529f8bb3b97a82f
shailaja <shailajapandiyan2005@gmail.com>
2025-07-24 13:06:32 +0530

    test commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route('/')
 def hello():
-    return "Haii i am shailu"
+    return 'Hello, Vercel !'
 
 if __name__=="__main__":
     app.run(debug=True)
